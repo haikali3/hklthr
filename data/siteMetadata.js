ce95dfe5ca6c459ee24094b68298a00430c09fa4
MUHAMMAD HAIKAL BIN MOHD TAHAR <muhammadhaikal.binmohdtahar@etiqa.com.my>
2025-04-10 21:53:54 +0800

    Refactor code structure for improved readability and maintainability

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Haikal Tahar`s Blog',
+  author: 'Haikal Tahar',
+  headerTitle: 'Haikal Tahar',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
