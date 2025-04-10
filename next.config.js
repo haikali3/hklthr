cb1719197eb2fa79c0c451ff222d07d271986530
haikali3 <manfdvcL9@gmail.com>
2025-04-11 00:53:01 +0800

    Update author information and enhance security policy for external media sources

--- next.config.js
+++ next.config.js
@@ -79,6 +79,10 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+        {
+          protocol: 'https',
+          hostname: 'media.licdn.com',
+        },
       ],
       unoptimized,
     },
